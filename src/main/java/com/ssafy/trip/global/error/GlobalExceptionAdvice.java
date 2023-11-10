70ee532cbaf34d76dd988c69212ab81ea2550e0f
JunginPark4364 <wjddlsee@gmail.com>
2023-11-11 03:22:30 +0900

    feat: security 예외 처리

--- src/main/java/com/ssafy/trip/global/error/GlobalExceptionAdvice.java
+++ src/main/java/com/ssafy/trip/global/error/GlobalExceptionAdvice.java
@@ -1,11 +1,31 @@
 package com.ssafy.trip.global.error;
 
+import com.ssafy.trip.global.dto.Response;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import static com.ssafy.trip.global.error.exception.ExceptionType.Authentication_EXCEPTION;
+import static com.ssafy.trip.global.error.exception.ExceptionType.FORBIDDEN_EXCEPTION;
+
 @RestControllerAdvice
 public class GlobalExceptionAdvice {
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity accessDeniedExceptionHandler(AccessDeniedException e) {
+        return ResponseEntity.badRequest()
+                .body(Response.fail(HttpStatus.FORBIDDEN.name(), FORBIDDEN_EXCEPTION.getErrorMessage()));
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity authenticationExceptionHandler(AuthenticationException e) {
+        return ResponseEntity.badRequest()
+                .body(Response.fail(HttpStatus.UNAUTHORIZED.name(), Authentication_EXCEPTION.getErrorMessage()));
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity handleExcpeiton(Exception ex) {
         ex.printStackTrace();
---
+++ src/main/java/com/ssafy/trip/global/error/exception/ExceptionType.java
@@ -0,0 +1,20 @@
+package com.ssafy.trip.global.error.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ExceptionType {
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다."),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "사용할 수 없는 토큰 입니다."),
+
+    CERTIFICATION_EXCEPTION(HttpStatus.UNAUTHORIZED, "자격 증명이 되어 있지 않습니다."),
+
+    FORBIDDEN_EXCEPTION(HttpStatus.FORBIDDEN, "권한이 없습니다."),
+    Authentication_EXCEPTION(HttpStatus.UNAUTHORIZED, "인증되지 않은 요청입니다.");
+
+    private final HttpStatus httpStatus;
+    private final String errorMessage;
+}
