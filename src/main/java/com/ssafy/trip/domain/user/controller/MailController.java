68c87b73cda2a3eb04e08091c13c7e9a3ff9d782
wjddlsee <wjddlsee@gmail.com>
2023-11-21 14:50:29 +0900

    feat: 메일에 전달할 링크 작성

--- src/main/java/com/ssafy/trip/domain/user/controller/MailController.java
+++ src/main/java/com/ssafy/trip/domain/user/controller/MailController.java
@@ -4,12 +4,14 @@ import com.ssafy.trip.domain.user.dto.EmailRequestDto;
 import com.ssafy.trip.domain.user.service.MailService;
 import com.ssafy.trip.global.dto.Response;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/mail")
@@ -18,6 +20,7 @@ public class MailController {
 
     @PostMapping("/password")
     public ResponseEntity sendPasswordChangeLink(@RequestBody EmailRequestDto emailRequestDto) {
+        log.debug("================ 입력받은 email {}", emailRequestDto.getEmail());
         mailService.sendPasswordEmail(emailRequestDto.getEmail());
 
         return ResponseEntity.ok(Response.success());
